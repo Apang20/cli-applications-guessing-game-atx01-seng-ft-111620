09ca32344b4efb609556c9681e43cba5e56c8b97
Avelon Pang <avelonpang@gmail.com>
2020-11-10 23:11:10 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@
 def run_guessing_game
   puts "Guess a number between 1 and 6."
   user_input = gets.chomp
-  comp_num = rand(6) 
+  comp_num = rand(6) + 1
   if user_input != "exit" 
     if user_input.to_i == comp_num
       puts "You guessed the correct number!"
