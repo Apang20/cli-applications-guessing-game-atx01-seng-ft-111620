a663fad24c6c47ab77aa6f5f08ebe3ff43874974
Avelon Pang <avelonpang@gmail.com>
2020-11-10 23:07:54 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -12,8 +12,8 @@ def run_guessing_game
     else
       puts "The computer guessed #{comp_num}."
     end
-    # comp_num = rand(1..6)
-    # user_input = gets.chomp
+    comp_num = rand(1..6)
+    user_input = gets.chomp
   end
   if user_input == "exit"
     puts "Goodbye!"
